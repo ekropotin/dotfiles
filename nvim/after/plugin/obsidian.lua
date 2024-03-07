b6d581f790f23543d65ca06025cde8cc72d5b0ee
Evgeny Kropotin <ekropotin@gmail.com>
2024-03-07 10:37:17 -0800

    Update Obsidian config

--- nvim/after/plugin/obsidian.lua
+++ nvim/after/plugin/obsidian.lua
@@ -7,17 +7,17 @@ local note_id_func = function(title)
     -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
     -- In this case a note with the title 'My new note' will be given an ID that looks
     -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-    local suffix = ""
+    local prefix = ""
     if title ~= nil then
         -- If title is given, transform it into valid file name.
-        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        prefix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
     else
         -- If title is nil, just add 4 random uppercase letters to the suffix.
         for _ = 1, 4 do
-            suffix = suffix .. string.char(math.random(65, 90))
+            prefix = prefix .. string.char(math.random(65, 90))
         end
     end
-    return tostring(os.time()) .. "-" .. suffix
+    return prefix .. "_" .. (os.time())
 end
 
 require('obsidian').setup({
@@ -27,15 +27,16 @@ require('obsidian').setup({
             path = obsidian_vault_path
         }
     },
-    callbacks = {
+    ui = {
+        enable = false
     },
     daily_notes = {
-        -- folder = "dailies"
+        folder = "dailies"
     },
     templates = {
         subdir = "templates"
     },
-    note_id_func
+    note_id_func = note_id_func
 });
 
 vim.keymap.set("n", "<leader>nn", vim.cmd.ObsidianNew, { desc = '[n]ote [n]ew' })
